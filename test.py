7562a9a0f91d123102617799eb52f663add568ab
jbbern1 <bernsteinmail@aol.com>
2023-05-09 19:03:55 -0400

    add function to gpt

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-import os
+import openai
+
+def call_gpt(prompt, api_key):      
+    openai.api_key = api_key
+    model_engine = 'text-davinci-003'
+    completion = openai.Completion.create(engine=model_engine, prompt=prompt, max_tokens=1024, n=1,stop=None,temperature=0.7)
+    message = completion.choices[0].text
+    return message
